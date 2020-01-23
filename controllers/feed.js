d37152324f97ee3a9221fcfb64b0f41f28d413e3
Luke Mueller <muelll79@hotmail.com>
2020-01-23 11:53:56 -0700

    first commit

---
+++ controllers/feed.js
@@ -0,0 +1,42 @@
+const { validationResult } = require('express-validator/check');
+
+exports.getPosts = (req, res, next) => {
+  res.status(200).json({
+    posts: [{
+      _id: '8',
+      title: 'First Post',
+      content: 'This is the first post!',
+      imageUrl: 'images/teapot.jpeg',
+      creator: {
+        name: 'Norm'
+      },
+      createdAt: new Date()
+    }]
+  });
+};
+
+exports.createPost = (req, res, next) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res
+      .status(422)
+      .json({ 
+        message: 'Validation failed', 
+        error: errors.array() 
+      })
+  }
+
+  const title = req.body.title;
+  const content = req.body.content;
+  // Create post in db
+  res.status(201).json({
+    message: 'Post created successfully!',
+    post: {
+      _id: new Date().toISOString(),
+      title: title,
+      content: content,
+      creator: { name: 'Jack' },
+      createdAt: new Date()
+    }
+  });
+};
